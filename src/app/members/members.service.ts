86ad0430bb74b321ea640f180257d99e92e76a32
Victor Avila <vicav99@gmail.com>
2023-07-18 14:36:43 -0700

    feat: create server and service

---
+++ src/app/members/members.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Member } from './member.model';
+
+const BASE_URL = 'http://localhost:3000';
+const path = 'members';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MembersService {
+  constructor(private http: HttpClient) {}
+
+  all(): Observable<Member[]> {
+    return this.http.get<Member[]>(this.getUrl());
+  }
+
+  load(id: number): Observable<Member> {
+    return this.http.get<Member>(this.getUrlForId(id));
+  }
+
+  create(member: Member): Observable<Member> {
+    return this.http.post<Member>(this.getUrl(), member);
+  }
+
+  update(member: Member): Observable<Member> {
+    return this.http.patch<Member>(this.getUrlForId(member.id), member);
+  }
+
+  delete(memberId: number): Observable<Member> {
+    return this.http.delete<Member>(this.getUrlForId(memberId));
+  }
+
+  private getUrl() {
+    return `${BASE_URL}/${path}`;
+  }
+
+  private getUrlForId(id: number) {
+    return `${this.getUrl()}/${id}`;
+  }
+}

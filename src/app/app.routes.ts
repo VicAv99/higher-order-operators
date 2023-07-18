ae93fe89b6a415d185afc5d47b385876d85e67ae
Victor Avila <vicav99@gmail.com>
2023-07-18 14:16:30 -0700

    feat: setup members route path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'members',
+    loadComponent: () =>
+      import('./members/members.component').then((m) => m.MembersComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '/members',
+  },
+];

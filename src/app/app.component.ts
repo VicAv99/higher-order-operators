d472b8f8244061c1926d7f826360106e620b5925
Victor Avila <vicav99@gmail.com>
2023-07-19 23:05:15 -0700

    feat: snackbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
@@ -16,7 +17,16 @@ import { RouterModule } from '@angular/router';
     MatIconModule,
     MatToolbarModule,
     MatListModule,
+    MatSnackBarModule,
     RouterModule,
   ],
 })
-export class AppComponent {}
+export class AppComponent {
+  snackBar = inject(MatSnackBar);
+
+  openSnackBar() {
+    this.snackBar.open('Made with 💖 by @victoravila__', 'Dismiss', {
+      duration: 3000,
+    });
+  }
+}
